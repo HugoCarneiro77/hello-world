5c3b1b5baaf353a6c33d7d18628c0aa63c78389a
HugoCarneiro77 <55037447+HugoCarneiro77@users.noreply.github.com>
2019-09-07 20:59:58 -0400

    Added HelloWorld.java

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+    /*
+     * Create a basic Java project in Eclipse with a single class called HelloWorld.
+     * This class should print out the string "Hello, World!"
+     * to the standard output stream using the System.out.println() method.
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+}
